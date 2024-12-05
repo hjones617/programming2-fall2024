75111fce95fa00f5195cca873f374cffe8d2ee87
Hugh Jones <hugh@Bens-MacBook-Air.local>
2024-12-05 14:47:09 -0600

    homework

---
+++ arrays/script.js
@@ -0,0 +1,17 @@
+//let tasks = ["go to gym", "do homework", "make dinner"];
+//console.log(tasks[0]);
+//
+//tasks[0] = "make dinner";
+//console.log(tasks);
+//
+//tasks.push("take a shower");
+//console.log(tasks);
+//
+//tasks.pop();
+//console.log(tasks);
+
+
+let groceries = ["milk", "fruit", "bread", "eggs"];
+groceries.splice(1, 1, "ice cream");
+grocies.splice(0, 1);
+alert(groceries);
