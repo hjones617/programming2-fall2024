035f1213cfac8f0a49ef94c858d17d40a14224d4
Hugh Jones <hugh@Bens-MacBook-Air.local>
2024-12-09 10:45:00 -0600

    velociraptor

---
+++ Javascript.loops/Script.js
@@ -0,0 +1,21 @@
+//let fastFood = ["hamburger", "fries", "milkshake", "chicken nuggets"];
+//for(let i = 0; i < fastFood.length; i++) {
+//    console.log(fastFood[i]);
+//}
+
+
+//
+//
+//
+//
+//let numbers = [45, 23, 78];
+//let doubled = [];
+//for(let n = 0; n < numbers.length; n++  ) {
+//    console.log(numbers[n]*2);
+//}
+
+
+
+for(let i = 0; i < 100; i++) {
+  console.log("Hugh")
+}
--- arrays/script.js
+++ arrays/script.js
@@ -7,11 +7,28 @@
 //tasks.push("take a shower");
 //console.log(tasks);
 //
-//tasks.pop();
-//console.log(tasks);
+////tasks.pop();
+////console.log(tasks);
+//
+//
+//let groceries = ["milk", "fruit", "bread", "eggs"];
+//groceries.splice(1, 1, "ice cream");
+//grocies.splice(0, 1);
+//alert(groceries);
+
+let items = ["phone", "shoes", "toothbrush"];
+console.log(items);
+items.push("computer", "basketball");
+console.log(items);
+items.pop("basketball");
+console.log(items);
+items.splice(1, "sink");
+console.log(items);
+items.splice(2, 1);
+console.log(items);
+
+let numbers = ["1", "2", "3"];
+numbers.splice(0, "4");
+numbers.splice(2, 1, "5");
 
 
-let groceries = ["milk", "fruit", "bread", "eggs"];
-groceries.splice(1, 1, "ice cream");
-grocies.splice(0, 1);
-alert(groceries);
