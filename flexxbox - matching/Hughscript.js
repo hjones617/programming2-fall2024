6aa824b5628b235a5f655e0ac98e0ef5bb8ff297
Hugh Jones <hugh@Bens-MacBook-Air.local>
2024-12-17 15:28:10 -0600

    message

---
+++ flexxbox - matching/Hughscript.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const pairs = [
+    {word: "el menú", match: ""}
+    ];
+});
---
+++ javascript-attributes/script.js
@@ -0,0 +1,21 @@
+let person = {
+  name: "Terry",
+  age: 23,
+  favoriteColor: "green",
+  height: "7'8\"",
+  satScore: 400,
+  job: "Basketball player",
+  biggestFear: "inflated basketballs",
+  favoriteNumber: 3
+};
+
+let person1 = {
+  name: "Jerry",
+  age: 23,
+  height: "7'8\"",
+  satScore: 400, 
+  job: "To stop Terry",
+  favoriteNumber: 2020
+};
+
+console.log("Once upon a time there was" + person.name + ". A star from the young age of" + person.favoriteNumber + ", everyone knew he was destined for greatness. but there was one out to stop him" + person1.name + "a evil cyborg clone of Terry was originally made to help Terry in basketball training. But" + person1.name + "went rogue. He primary goal is" + person1.job + "from playing professional" + person.job );
